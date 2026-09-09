3616587d564c079b980a96ab9c6821d53a132d7a
Yikwab Andeyantso <yikwaba@users.noreply.github.com>
2026-09-09 06:46:22 +0200

    chore: stop tracking Next.js build artifacts (.next)

--- apps/portal/.next/static/chunks/app/layout-34b96c75c498b6e5.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{2788:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,9653,23))},9653:()=>{}},e=>{e.O(0,[686,710,385,358],()=>e(e.s=2788)),_N_E=e.O()}]);
